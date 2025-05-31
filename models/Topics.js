d6a41cbb7b3cde27fd04b26be8c96654818d9b70
PromiseP <khanpennpromise@gmail.com>
2025-05-31 11:45:49 +0100

    login and registration functional

---
+++ models/Topics.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const topicSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        require: true,
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.Model('Topics', topicSchema);
